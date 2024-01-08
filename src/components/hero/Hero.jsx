616681a91097e58d85a531faf7cd05a5db3a85d1
Mario Garcia <themariogarcia@gmail.com>
2024-01-08 15:07:31 -0600

    added hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import './hero.scss'
+import { motion } from 'framer-motion'
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+}
+
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: '-100%', // high number bigger than screen
+    transition: {
+      repeat: Infinity,
+      repeatType: 'mirror',
+      duration: 10,
+    },
+  },
+}
+
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>Mario Garcia</motion.h2>
+          <motion.h1 variants={textVariants}>Frontend Developer</motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              See the Latest Work
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Software Developer
+      </motion.div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" />
+      </div>
+    </div>
+  )
+}
